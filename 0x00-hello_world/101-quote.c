0bd5d7e2661fd3d5f34f85f9094f1345c22231e6
shegami <mohcinela1998@gmail.com>
2023-03-16 10:06:15 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <errno.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Faillure)
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
