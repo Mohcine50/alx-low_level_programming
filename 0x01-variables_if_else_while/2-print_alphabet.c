ab29c732fb8816ae2a522ccb7a3593a93a313bfa
shegami <mohcinela1998@gmail.com>
2023-03-17 09:04:18 -0700

    last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 97;
+	while(c >= 97 && c <= 122)
+	{
+		putchar(c);
+		c++;
+	}
+	printf("\n");
+	return (0);
+}
