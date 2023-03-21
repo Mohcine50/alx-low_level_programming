668385849b217cd4ae916e1426c29115525b8905
shegami <mohcinela1998@gmail.com>
2023-03-21 16:24:53 -0700

    print sign of number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ *
+ * @n: number that we print his last digit
+ *
+ * Return: the value of last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
