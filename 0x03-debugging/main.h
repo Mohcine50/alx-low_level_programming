b93d45c6e8c77b69a2e04f40b78b9994ae889223
shegami <mohcinela1998@gmail.com>
2023-03-23 09:09:59 -0700

    OK

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,18 +1,11 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
