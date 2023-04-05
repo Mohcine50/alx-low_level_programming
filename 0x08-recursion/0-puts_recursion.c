5ac30d9eb2fc0eae01df1e170939ed9fd58a0fe8
shegami <mohcinela1998@gmail.com>
2023-04-05 15:37:07 -0700

    print string recu

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string with recursion
+ * @s: string to print
+ * Return: none
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+
+	if (*s != '\0')
+		_puts_recursion(s + 1);
+}
