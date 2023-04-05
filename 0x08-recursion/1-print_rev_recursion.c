1a6e5229f8555bb108443088f0b5c0dbef577d31
shegami <mohcinela1998@gmail.com>
2023-04-05 16:10:16 -0700

    print string recu

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string with recursion in rev
+ * @s: string to print
+ * Return: none
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+			}
+}
