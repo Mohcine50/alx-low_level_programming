b3eb042f72795301954a72a4c976aae340464a00
shegami <mohcinela1998@gmail.com>
2023-04-06 15:14:51 -0700

    OK

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: number of args
+ * @argv: array of args
+ * Return: 0
+ *
+ */
+int main(int argc, char **argv)
+{	
+	if (argc == 1)
+		printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+ArgC argV Tasks
