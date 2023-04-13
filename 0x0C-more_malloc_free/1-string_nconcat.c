815da8b1454d6228df2b6a0a4fbc62b615c63ad3
shegami <mohcinela1998@gmail.com>
2023-04-13 16:25:15 -0700

    malloc_checked

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	n = (n >= slen2) ? slen2 : n;
 	ptr = (char *)malloc(sizeof(char) * (slen1 + n) + 1);
 
-	if (s1 == NULL)
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
