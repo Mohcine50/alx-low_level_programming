462f6c2e8e705c40522bdda703a2cc612e2445a4
shegami <mohcinela1998@gmail.com>
2023-04-27 04:01:55 -0700

    fix betty

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <stddef.h>
+
+/**
+ * struct list - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list
 {
 	char *str;
