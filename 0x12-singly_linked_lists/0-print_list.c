e2e007d4112f58119ce7364f22330bd347d258ee
shegami <mohcinela1998@gmail.com>
2023-04-27 11:53:16 +0100

    fix betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,18 +1,19 @@
 #include "lists.h"
+
 /**
-* print_list - print a the element of the list
-* @list_t:the head or starting point for the print
-* Return: number of the elements
-*/
+ * print_list - print a the element of the list
+ * @list_t:the head or starting point for the print
+ * Return: number of the elements
+ */
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
-	
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-		printf("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		}
 		else
 			printf("[%u] %s\n", h->len, h->str);
